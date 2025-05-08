c840ba598a65bca08f11ef2af2aae3f9bdabd3d2
Yevheniia <marynchak2005@gmail.com>
2025-05-08 13:38:04 +0300

    Commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import { useAuth } from "../context/AuthProvider";
-import { Link, useNavigate } from "react-router-dom";
-
+import { useNavigate } from "react-router-dom";
+N
 const Header = () => {
   const { user, logout } = useAuth();
   const navigate = useNavigate();
@@ -18,17 +18,20 @@ const Header = () => {
     <header className="mx-0 sm:mx-auto sm:max-w-8xl px-4 sm:px-6 lg:px-8 w-full font-jomhuria">
       <div className="flex justify-between items-center pt-4 pb-1 px-4 sm:px-6 lg:px-8 border-b border-white">
         <h1 className="text-4xl sm:text-6xl lg:text-7xl">
-          <Link to="/">My Travel</Link>
+          <a href="/">My Travel</a>
         </h1>
         <nav>
           <ul className="flex flex-wrap items-center gap-4 sm:gap-8 lg:gap-12 text-xl sm:text-2xl md:text-4xl">
             <li className="hover:text-[#b7c6d0] transition-colors duration-300 ease-in-out">
-              <Link to="/publications">Articles</Link>
+              <a href="/publications">Articles</a>
             </li>
             {user ? (
               <>
                 <li className="hover:text-[#b7c6d0] transition-colors duration-300 ease-in-out">
-                  <Link to="/profile">My Publications</Link>
+                  <a href="/profile">My Publications</a>
+                </li>
+                <li className="hover:text-[#b7c6d0] transition-colors duration-300 ease-in-out">
+                  <a href="/likes">Likes</a>
                 </li>
                 <li className="hover:text-[#b7c6d0] transition-colors duration-300 ease-in-out">
                   <button onClick={handleLogout}>Logout</button>
@@ -36,7 +39,7 @@ const Header = () => {
               </>
             ) : (
               <li className="hover:text-[#b7c6d0] transition-colors duration-300 ease-in-out">
-                <Link to="/login">Login</Link>
+                <a href="/login">Login</a>
               </li>
             )}
           </ul>
